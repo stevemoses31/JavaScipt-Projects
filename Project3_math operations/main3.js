865ce8cfe3bc5ca5ebd19db64ff0c5578e704ce4
stevemoses31 <134295998+stevemoses31@users.noreply.github.com>
2023-07-24 08:08:47 -0700

    a

---
+++ Project3_math operations/main3.js
@@ -0,0 +1,17 @@
+let x = 6;
+let y = 2;
+let z = x + y;
+let a = x % y;
+let jacob = 10;
+jacob++
+let jose = 12;
+jose--
+
+
+document.write(z)
+document.write(x * y)
+document.write(x-y)
+document.write(a)
+
+document.write(jacob)
+document.write(jose)
