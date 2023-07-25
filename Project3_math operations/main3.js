aa355ee2851fbdf3843aa88894e0e34bef9eaa2a
stevemoses31 <134295998+stevemoses31@users.noreply.github.com>
2023-07-25 11:43:20 -0700

    a

--- Project3_math operations/main3.js
+++ Project3_math operations/main3.js
@@ -1,30 +1,37 @@
-var x = 5; 
-x++; 
-document.write(x)
+function plusFunction() { 
+    var answer = 2+2;
+    document.getElementById("plus").innerHTML = answer;
+}
 
-var x = 4.25; 
-x--;
-document.write(x)
+function minusFunction() { 
+    var answer = 5-3;
+    document.getElementById("minus").innerHTML = answer;
+}
 
-var x = 7;
-var y = 5;
-x-y;
-document.write(x-y)
+function multiplyFunction() { 
+    var answer = 5*2;
+    document.getElementById("multiply").innerHTML = answer;
+}
 
-var x = 3; 
-var y = 10; 
-x*y; 
-document.write(x*y)
+function divisionFunction() { 
+    var answer = 10%3;
+    document.getElementById("division").innerHTML = answer;
+}
 
-var x = 15; 
-var y = 10; 
-x+y; 
-document.write(x+y)
+function plusPlusFunction() { 
+    var a = 5;
+    a++;
+    var answer = a;
+    document.getElementById("plusplus").innerHTML = answer;
+}
 
-var x = 20; 
-var y = 2; 
-x%y;
-document.write(x%y)
+function minusMinusFunction() { 
+    var b = 7;
+    b--;
+    var answer = b;
+    document.getElementById("minusminus").innerHTML = answer;
+}
+
 
 window.alert(Math.random());
 window.alert(Math.random() * 100);
