49e9986597b9c4d22820e5189ae4b7aef7c333f4
stevemoses31 <134295998+stevemoses31@users.noreply.github.com>
2023-07-25 10:27:50 -0700

    a

--- Project 2_functions/main2.js
+++ Project 2_functions/main2.js
@@ -1,5 +1,6 @@
 function myFunction() { 
-    var sentence = "The Tech Academy"; sentence += "is an amazing school!"; 
+    var sentence = "The Tech Academy"; 
+    sentence += "is an amazing school!"; 
     document.getElementById("Concatenante").innerHTML = sentence;
 }
 
