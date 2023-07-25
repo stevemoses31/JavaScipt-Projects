43316f4a73eed0633423e3f282bba8e4a8c0914a
stevemoses31 <134295998+stevemoses31@users.noreply.github.com>
2023-07-24 21:05:15 -0700

    Update main3.js

--- Project3_math operations/main3.js
+++ Project3_math operations/main3.js
@@ -1,18 +1,30 @@
-let x = 6;
-let y = 2;
-let z = x + y;
-let a = x % y;
-let jacob = 10;
-jacob++
-let jose = 12;
-jose--
-window.alert(Math.random() * 100;
+var x = 5; 
+x++; 
+document.write(x)
 
+var x = 4.25; 
+x--;
+document.write(x)
 
-document.write(z)
-document.write(x * y)
+var x = 7;
+var y = 5;
+x-y;
 document.write(x-y)
-document.write(a)
 
-document.write(jacob)
-document.write(jose)
+var x = 3; 
+var y = 10; 
+x*y; 
+document.write(x*y)
+
+var x = 15; 
+var y = 10; 
+x+y; 
+document.write(x+y)
+
+var x = 20; 
+var y = 2; 
+x%y;
+document.write(x%y)
+
+window.alert(Math.random());
+window.alert(Math.random() * 100);
