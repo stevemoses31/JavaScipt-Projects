1179e798dc7c998cc8d03faf77a3d73a436dacfa
stevemoses31 <134295998+stevemoses31@users.noreply.github.com>
2023-07-28 19:52:01 -0700

    a

---
+++ Project5_comparisons/main5.js
@@ -0,0 +1,26 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20>8);
+}
+
+document.write(5==5)
+
+x = 25;
+y = 25;
+document.write(x === y)
+
+A = "Earth";
+B = "Earth";
+document.write(A === B)
+
+document.write(15>5)
+document.write(10<2)
+
+document.write(7 > 3 && 12 > 4)
+
+document.write(4 > 8 || 13 > 5)
+
+console.log(2+2); 
